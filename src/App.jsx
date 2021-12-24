f7b7e5b8d8053517d8d586e7d6c56a80577d7934
supminn <30731236+supminn@users.noreply.github.com>
2021-12-24 20:10:17 +0530

    setup webpack with react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1>Welcome to React</h1>
+      <p>This is a react app build using webpack config along with babel</p>
+    </>
+  );
+}
