ff59577afecd2115c6296694f5d5fec94f491ff3
supminn <30731236+supminn@users.noreply.github.com>
2021-12-30 22:45:25 +0530

    dev server config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
-    <>
+    <div className="App">
       <h1>Welcome to React</h1>
-      <p>This is a react app build using webpack config along with babel</p>
-    </>
+      <div>
+        <img
+          className="logo-webpack"
+          src="https://webpack.js.org/site-logo.1fcab817090e78435061.svg"
+          alt="webpack icon"
+        />
+        <p className="txt-desc">
+          This is a react app build using webpack config along with babel.
+        </p>
+      </div>
+    </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,44 @@
 const path = require("path");
+
 module.exports = {
-	entry:"./src/index.js",
-	output:{
-		filename:"bundle.js",
-		path: path.join(__dirname, "public")
-	},
-	module:{
-		rules: [{
-			loader: "babel-loader",
-			test:/\.js[x]?$/,
-			exclude: /node_modules/
-		}]
-	},
-	mode: "development"
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "public"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  mode: "production",
+  devtool: "inline-source-map",
+  devServer: {
+    // open: true,  open's a new tab on browser which runs localhost
+    client: {
+      reconnect: 3,
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
 };
